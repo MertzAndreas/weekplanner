d2921b1c74bf2e9440ad0b2797cf8c71f1b924e4
MertzAndreas <andreasmertz11@gmail.com>
2024-09-25 13:40:52 +0200

    Hello

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href={"/example"}>Ref to example</Link>
+    </View>
+  );
+}
